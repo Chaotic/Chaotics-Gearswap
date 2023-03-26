b4da0dc73b627d387935a0d6f67e7e855b1ffb9b
Chaotic <matt.scoville@gmail.com>
2023-03-26 12:07:34 -0400

    Added hud (very early) added brd

---
+++ data/brd.lua
@@ -0,0 +1,189 @@
+function get_sets()
+
+  mote_include_version = 2
+
+  include('Mote-Include.lua')
+  
+end
+
+function job_setup()
+
+  include('chaotic-Include.lua')
+
+  state.Buff['Pianissimo'] = buffactive['Pianissimo'] or false
+  state.Buff['Nightingale'] = buffactive['Nightingale'] or false
+  state.Buff['Troubadour'] = buffactive['Troubadour'] or false
+
+  initialize_job()
+
+end
+
+function user_setup()
+
+  state.IdleMode:options('Normal','Combat')
+  state.OffenseMode:options('Normal','DW')
+  state.WeaponskillMode:options('Normal','HNM')
+  state.PhysicalDefenseMode:options('PDT')
+  state.MagicalDefenseMode:options('MDT')
+  state.CastingMode:options('Normal','Acc','Combat')
+
+  sleeps            = S{'Sleep', 'Sleep II', 'Sleepga', 'Sleepga II', 'Bind'}
+  elemental_debuffs = S{'Burn','Choke','Shock','Rasp','Drown','Frost'}
+
+  spikes            = S{'Blaze Spikes','Shock Spikes','Ice Spikes'}
+  
+  bard_debuffs      = S{'Lullaby','Elegy','Requiem','Threnody'}
+  minne = S{'Knight\'s Minne','Knight\'s Minne II','Knight\'s Minne III','Knight\'s Minne IV','Knight\'s Minne V',}
+  minne = S{'Valor Minuet','Knight\'s Minne II','Knight\'s Minne III','Knight\'s Minne IV','Knight\'s Minne V',}
+  minne = S{'Knight\'s Minne','Knight\'s Minne II','Knight\'s Minne III','Knight\'s Minne IV','Knight\'s Minne V',}
+  minne = S{'Knight\'s Minne','Knight\'s Minne II','Knight\'s Minne III','Knight\'s Minne IV','Knight\'s Minne V',}
+
+  casting_mode = ''
+
+  send_command('bind !f9 gs c cycle IdleMode')
+
+end
+
+
+
+
+-- Auto-Pianissimo
+function job_precast(spell,action,spellMap,eventArgs)
+  
+  if spell.type == 'BardSong' then
+    if ((spell.target.type == 'PLAYER' and not spell.target.charmed) or (spell.target.type == 'NPC' and spell.target.in_party)) and not state.Buff['Pianissimo'] then
+
+      local spell_recasts = windower.ffxi.get_spell_recasts()
+      if spell_recasts[spell.recast_id] < 2 then
+
+        send_command('@input /ja "Pianissimo" <me>; wait 1.5; input /ma "'..spell.name..'" '..spell.target.name)
+        eventArgs.cancel = true
+        return
+
+      end
+
+    end
+    
+  end
+
+end
+
+-- Enfeebling magic handling
+function job_midcast(spell,action,spellMap,eventArgs)
+
+  if spell.skill == 'Enfeebling Magic' then
+    equip(sets.midcast[spell.skill][spell.type])
+  else
+    equip(sets.midcast[spell.skill])
+  end
+
+end
+
+--[[ Don't like doing things this way
+-- Obi and grip handling
+function job_post_midcast(spell,action,spellMap,eventsArgs)
+
+  if spellMap == 'Cure' then
+    equip({main="Chatoyant staff",sub="Raptor leather strap +1"})
+  else
+    if player.status == 'Idle' and state.IdleMode.value == 'Normal' and state.CastingMode.value ~= 'Combat' then
+      
+      if spell.skill == 'Enfeebling Magic' then
+        
+        if state.CastingMode.value == 'Normal' then
+          equip({main="Alkalurops"})
+        else
+          equip({main="Chatoyant staff"})
+        end
+        
+        if spell.type == 'WhiteMagic' then
+          equip({sub="Raptor leather strap +1"})
+        elseif spell.type == 'BlackMagic' then
+          equip({sub="Bugard leather strap +1"})
+        end 
+        
+      elseif spell.type == 'BardSong' then
+        equip({main="Chatoyant staff",sub="Bugard leather strap +1"})
+      elseif spell.english == 'Stoneskin' then
+        equip({main="Alkalurops"})
+      end
+      
+      if state.CastingMode.value == 'Acc' and player.status ~= 'Engaged' then
+        equip(sets.grip[spell.element])
+      end
+      
+    end
+  end
+
+  if spell.skill ~= 'Enhancing Magic' or spell.skill ~= 'Enfeebling Magic' then
+    if spell.element == world.weather_element or spell.element == world.day_element then
+      
+      if world.weather_element ~= 'Dark' and spellMap == 'Cure' then
+        equip({waist="Hachirin-no-obi"})
+      end
+      
+      if spell.skill == 'Elemental Magic' then
+        equip({waist="Hachirin-no-obi"})
+      end
+      
+    end
+  end
+  
+end
+]]
+
+function job_get_spell_map(spell,spellMap,default_spell_map)
+
+  if spell.skill == 'Enfeebling Magic' and sleeps:contains(spell.english) then
+    return 'Sleep'
+  elseif spell.skill == 'Elemental Magic' and elemental_debuffs:contains(spell.english) then
+    return 'EleEnfeebs'
+  elseif spell.skill == 'Enhancing Magic' and spikes:contains(spell.english) then
+    return 'Spikes'
+  elseif bard_debuffs:contains(spellMap) or spell.english == 'Magic Finale' then
+    return 'BardDebuff'
+  end
+ end
+
+function job_status_change(new,old)
+
+  if new == 'Engaged' then
+    send_command('gs c set IdleMode Combat;gs c set CastingMode Combat')
+  end
+  
+end
+
+function job_buff_change(name,gain)
+
+    handle_standard_buffs(buff,gain)
+  
+  if name == 'Elemental Seal' then
+    if gain then
+      casting_mode = state.CastingMode.value
+      if (state.CastingMode.value ~= 'Normal' or state.CastingMode.value ~= 'Combat') then
+        send_command('gs c set CastingMode Normal')
+      end
+    else
+      send_command('gs c set CastingMode '..casting_mode)
+    end
+  end
+
+end
+
+function customize_idle_set(idleSet)
+
+--[[ I don't have this
+  if daytime then 
+    idleSet = set_combine(idleSet,{waist="Lycopodium sash"})
+  end
+  ]]
+  crafting_mode = state.CraftingMode.value
+--[[ This isn't applicable right now
+  if (state.IdleMode.value == 'Normal' and state.CastingMode.value ~= 'Combat' and state.CastingMode.value ~= 'Combat') then 
+    idleSet = set_combine(idleSet,{main=gear.Staff.PDT,sub=gear.ElementalGrip})
+  end
+]]
+  idleSet = maybe_equip_crafting(idleSet,crafting_mode)
+  return idleSet
+  
+end
---
+++ data/gear/asen_brd.lua
@@ -0,0 +1,160 @@
+-- Define sets and vars used by this job file.
+function init_gear_sets()
+
+  windower.send_command('sta !packets on')
+-- misc
+sets.MND = {}
+
+sets.INT = {}
+
+sets.CHR = {}
+
+sets.Singing = {}
+sets.Wind = set_combine(sets.Singing,{})
+sets.String = set_combine(sets.Singing,{})
+--  These will be filled in as I get instruments / spells
+sets.midcast['Minne'] = set_combine(sets.midcast.FastRecast,sets.String,sets.CHR,{
+range="Harp +1",
+})
+sets.midcast['Requiem'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Flute +2",
+})
+sets.midcast['Threnody'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Piccolo +1",
+})
+sets.midcast['Minuet'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Cornette +2",
+})
+sets.midcast['Lullaby'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Mary's horn",
+})
+sets.midcast['March'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Ryl.Spr. Horn",
+})
+sets.midcast['Mambo'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Gemshorn +1",
+})
+sets.midcast['Madrigal'] = sets.midcast['Requiem']
+sets.midcast['Prelude'] = sets.midcast['Requiem']
+sets.midcast['Etude'] = sets.midcast['Requiem']
+sets.midcast['Ballad'] = sets.midcast['Requiem']
+sets.midcast['Paeon'] = sets.midcast['Minne']
+sets.midcast['Carol'] = sets.midcast['Requiem']
+sets.midcast['Elegy'] = sets.midcast['Requiem']
+--[[
+sets.midcast['Madrigal'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Prelude'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Etude'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Ballad'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Paeon'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Carol'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+sets.midcast['Elegy'] = set_combine(sets.midcast.FastRecast,sets.Wind,sets.CHR,{
+range="Maple Harp +1",
+})
+]]
+
+-- idle
+sets.idle = {
+  main="Bronze Sword",
+  range="Cornette +2",
+  body="Chocobo Shirt",
+  hands="Mithran Gauntlets",
+  legs="Mithran Loincloth",
+  feet="Mithran Gaiters",
+  left_ring="Warp Ring",
+  right_ring="Anniversary Ring",
+}
+   
+sets.idle.Town = set_combine(sets.idle,
+                  {
+                      back="Nexus cape",
+                      left_ring="Warp ring"
+                  })
+
+sets.resting = {}
+
+-- precast magic
+sets.precast.FC = {}
+
+sets.precast.FC['BardSong'] = set_combine(sets.precast.FC,{})
+
+-- midcast magic
+sets.midcast.FastRecast = set_combine(sets.precast.FC,{})
+               
+sets.midcast['Utsusemi: Ni'] = set_combine(sets.midcast.FastRecast,{})
+ 
+sets.midcast['Utsusemi: Ichi'] = set_combine(sets.midcast['Utsusemi: Ni'],{})
+
+                                                   
+sets.midcast['Healing Magic'] = set_combine(sets.MND,sets.midcast.FastRecast,{})
+                                                   
+sets.midcast['Divine Magic'] = set_combine(sets.MND,{})
+               
+sets.midcast['Divine Magic'].Acc = set_combine(sets.midcast['Divine Magic'],{})
+                                                   
+sets.midcast['Enfeebling Magic'] = {}
+                                                   
+sets.midcast['Enfeebling Magic']['BlackMagic'] = set_combine(sets.INT,{})
+ 
+sets.midcast['Enfeebling Magic']['BlackMagic'].Acc = set_combine(sets.midcast['Enfeebling Magic']['BlackMagic'],sets.midcast['Enfeebling Magic'],{})
+
+sets.midcast['Enfeebling Magic']['WhiteMagic'] = set_combine(sets.MND,{})
+ 
+sets.midcast['Enfeebling Magic']['WhiteMagic'].Acc = set_combine(sets.midcast['Enfeebling Magic']['WhiteMagic'],sets.midcast['Enfeebling Magic'],{})
+
+sets.midcast['Elemental Magic'] = set_combine(sets.INT,
+              {
+                  ear2="Moldavite earring",
+                  hands="Zenith mitts"
+              })
+ 
+sets.midcast['Elemental Magic'].Acc = set_combine(sets.midcast['Elemental Magic'],{})
+                                                   
+sets.midcast['Dark Magic'] = set_combine(sets.INT,{})
+               
+sets.midcast['Dark Magic'].Acc = set_combine(sets.midcast['Dark Magic'],{})
+                                                   
+sets.midcast['Enhancing Magic'] = set_combine(sets.midcast.FastRecast,{})
+                                                   
+-- custom midcast magic                            
+sets.midcast['Stoneskin'] = set_combine(sets.MND,{})
+               
+sets.midcast.Cure = set_combine(sets.midcast['Healing Magic'],{})
+               
+sets.midcast.Curaga = set_combine(sets.midcast.Cure,{})
+
+sets.midcast['Cursna'] = set_combine(sets.midcast['Healing Magic'],{})
+               
+-- bard songs
+sets.midcast['BardSong'] = set_combine(sets.midcast.FastRecast,sets.CHR,{})
+               
+sets.midcast.BardDebuff = set_combine(sets.CHR,{})
+
+-- Defense sets
+sets.defense.PDT = {}
+   
+sets.defense.MDT = {}
+
+-- Engaged sets
+sets.engaged = {}
+
+-- WS sets
+sets.precast.WS = {}
+ 
+sets.precast.WS.Acc = set_combine(sets.precast.WS,{})
+
+  
+    initialize_crafting_sets(player.name)
+end
